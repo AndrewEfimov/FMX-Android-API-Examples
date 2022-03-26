d91e3145f03d62c2c729e9cda64cd548c28d97f8
AndrewEfimov <infocean@gmail.com>
2022-03-26 13:29:10 +0300

    Wifi test

---
+++ WifiTestAPI/FMXWifiTestAPI.dpr
@@ -0,0 +1,15 @@
+﻿program FMXWifiTestAPI;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Form.Main in 'Form.Main.pas' {FormMain},
+  uUniversalReceiver in 'class\uUniversalReceiver.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.Run;
+end.
